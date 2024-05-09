09f55b911ea428cd314eb33fe30e6126f822a0b3
Reviewaa <reviewaa8@gmail.com>
2024-05-09 14:47:56 +0530

    Minor updates

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -7,7 +7,7 @@ const tweetRouter = Router();
 // Define route paths
 
 tweetRouter.get("/:tweetId", getTweetController)
-tweetRouter.get("", getAllTweetsController)
+tweetRouter.get("/", getAllTweetsController)
 tweetRouter.post("/", createTweetController)
 tweetRouter.delete("/:tweetId", deleteTweetController)
 tweetRouter.put("/", updateTweetController)
